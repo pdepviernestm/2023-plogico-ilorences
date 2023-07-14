2ad73532ffa194e1d763994876eec2fa3eb7d1d5
ilorences <ilorences@frba.utn.edu.ar>
2023-07-14 09:25:20 -0300

    Add files via upload
    Commit inicial

---
+++ main.pl
@@ -0,0 +1,63 @@
+%apareceEn( Personaje, Episodio, Lado de la luz).
+apareceEn( luke, elImperioContrataca, luminoso).
+apareceEn( luke, unaNuevaEsperanza, luminoso).
+apareceEn( vader, unaNuevaEsperanza, oscuro).
+apareceEn( vader, laVenganzaDeLosSith, luminoso).
+apareceEn( c3po, laAmenazaFantasma, luminoso).
+apareceEn( c3po, unaNuevaEsperanza, luminoso).
+apareceEn( c3po, elImperioContrataca, luminoso).
+apareceEn( chewbacca, elImperioContrataca, luminoso).
+apareceEn( yoda, elAtaqueDeLosClones, luminoso).
+apareceEn( yoda, elAtaqueDeLosClones, luminoso).
+
+%Maestro(Personaje)
+maestro(luke).
+maestro(leia).
+maestro(vader).
+maestro(yoda).
+maestro(rey).
+maestro(duku).
+
+%caracterizacion(Personaje,Aspecto).
+%aspectos:
+% ser(Especie,Tamaño)
+% humano
+% robot(Forma)
+caracterizacion(chewbacca,ser(wookiee,10)).
+caracterizacion(luke,humano).
+caracterizacion(vader,humano).
+caracterizacion(yoda,ser(desconocido,5)).
+caracterizacion(jabba,ser(hutt,20)).
+caracterizacion(c3po,robot(humanoide)).
+caracterizacion(bb8,robot(esfera)).
+caracterizacion(r2d2,robot(secarropas)).
+
+%elementosPresentes(Episodio, Dispositivos)
+elementosPresentes(laAmenazaFantasma, [sableLaser]).
+elementosPresentes(elAtaqueDeLosClones, [sableLaser, clon]).
+elementosPresentes(laVenganzaDeLosSith, [sableLaser, mascara, estrellaMuerte]).
+elementosPresentes(unaNuevaEsperanza, [estrellaMuerte, sableLaser, halconMilenario]).
+elementosPresentes(elImperioContrataca, [mapaEstelar, estrellaMuerte] ).
+
+
+%precede(EpisodioAnterior,EpisodioSiguiente)
+precedeA(laAmenazaFantasma,elAtaqueDeLosClones).
+precedeA(elAtaqueDeLosClones,laVenganzaDeLosSith).
+precedeA(laVenganzaDeLosSith,unaNuevaEsperanza).
+precedeA(unaNuevaEsperanza,elImperioContrataca).
+
+
+%Desarrollo:
+
+nuevoEpisodio(Heroe, Villano, Extra, Dispositivo) :-
+    personajesDiferentes(Heroe,Villano,Extra),
+    jediPerpetuo(Heroe).
+
+
+personajesDiferentes(Personaje1,Personaje2,Personaje3):-
+    Personaje1\=Personaje2,
+    Personaje1\=Personaje3,
+    Personaje2\=Personaje3.
+
+jediPerpetuo(Jedi) :-
+    
